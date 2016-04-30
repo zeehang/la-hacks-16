e22954e895f9c8aea3fcef600274213046454c16
zeehang <myque@live.com>
2016-04-30 16:52:55 -0700

    added server trying

--- test.php
+++ test.php
@@ -7,8 +7,8 @@
 <?php
 echo "Hello World!";
 
-$servername = "lahacks16mike";
-$username = "mszhang";
+$servername = "us-cdbr-azure-west-c.cloudapp.net";
+$username = "us-cdbr-azure-west-c.cloudapp.net";
 $password = "54";
 
 $conn = new mysqli($servername, $username, $password);
