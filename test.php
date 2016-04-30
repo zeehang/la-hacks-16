9abd2e80db1e5d28bb5bd45a146fa0ef683a0030
zeehang <myque@live.com>
2016-04-30 19:29:46 -0400

    server testing

--- test.php
+++ test.php
@@ -6,6 +6,19 @@
 
 <?php
 echo "Hello World!";
+
+$servername = "lahacks16mike";
+$username = "mszhang";
+$password = "54";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn -> connect_error)
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected succesfully";
+
 ?>
 
 </body>
